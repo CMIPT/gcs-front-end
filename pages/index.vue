f112538c48883f98db4788c80b0a0fb680e20def
nonumx <nonumx@gmail.com>
2024-10-15 14:52:56 +0800

    Introduce Arco Design

--- pages/index.vue
+++ pages/index.vue
@@ -1,36 +1,5 @@
 <script setup>
-const userAuth = useState("userAuth");
 
-const fetchUserInfo = async () => {
-  // 没有登录信息需要用户手动登录
-  if (!userAuth.value) {
-    return false;
-  }
-
-  try {
-    const config = useRuntimeConfig();
-    const accessToken = userAuth.value["access-token"];
-    const { data } = await $fetch(`${config.public.apiBaseURL}/gcs/user/get`, {
-      headers: {
-        "Access-Token": accessToken,
-      },
-    });
-    console.log(data);
-  } catch (e) {
-    console.error(e);
-    return false;
-  }
-  return true;
-};
-
-const fetchUserRepo = async () => {};
-
-const fetchHomeData = async () => {
-  if (await fetchUserInfo()) {
-    fetchUserRepo();
-  }
-};
-fetchHomeData();
 </script>
 
 <template>
--- pages/signup.vue
+++ pages/signup.vue
@@ -41,19 +41,21 @@ const handleSignup = async () => {
 <template>
   <div class="flex flex-col justify-center h-screen">
     <div
-      class="border border-slate-200 rounded-lg shadow-lg mx-auto flex flex-col gap-2 px-6 py-10"
+      class="border border-slate-200 rounded-lg shadow-lg mx-auto flex flex-col gap-2 px-6 pb-10"
       :style="{ width: signupWidth }"
     >
-      <h1 class="text-2xl text-center pb-4">注册</h1>
-      <span>用户名</span>
-      <GInput type="text" v-model:model-value="username" />
-      <span>邮箱</span>
-      <GInput type="text" v-model:model-value="email" />
-      <span>密码</span>
-      <GInput type="password" v-model:model-value="userPassword" />
-      <span>请确认密码</span>
-      <GInput type="password" v-model:model-value="userPasswordConfirm" />
-      <GButton class="ml-auto mt-4" @click="handleSignup">注册</GButton>
+      <ATypographyTitle class="text-center pb-4">
+        注册
+      </ATypographyTitle>
+      <ATypographyText>用户名</ATypographyText>
+      <AInput v-model:model-value="username" />
+      <ATypographyText>邮箱</ATypographyText>
+      <AInput v-model:model-value="email" />
+      <ATypographyText>密码</ATypographyText>
+      <AInput type="password" v-model:model-value="userPassword" />
+      <ATypographyText>请确认密码</ATypographyText>
+      <AInput type="password" v-model:model-value="userPasswordConfirm" />
+      <AButton class="ml-auto mt-4" @click="handleSignup">注册</AButton>
     </div>
   </div>
 </template>
