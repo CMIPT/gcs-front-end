275cca14ec4b428a7b19eb972228b1cf87dc30fd
nonumx <nonumx@gmail.com>
2024-10-24 23:05:53 +0800

    WIP: request failed on both create and validate

--- pages/new.vue
+++ pages/new.vue
@@ -4,10 +4,80 @@ const newRepositoryWidth = "600px";
 const newRepositoryForm = reactive({
   repositoryName: "",
   repositoryDescription: "",
-  isPrivate: false,
 });
 
-const handleNewRepository = () => {};
+const repositoryIsPrivate = ref(false);
+
+const userAuth = useState("userAuth");
+const userInfo = useState("userInfo");
+
+const handleNewRepository = async () => {
+  const config = useRuntimeConfig();
+  try {
+    Message.loading("正在新建仓库...");
+    const apiURL = new URL(
+      APIPaths.REPOSITORY_CREATE_REPOSITORY_API_PATH,
+      config.public.apiBaseURL
+    );
+    await $fetch(apiURL, {
+      method: "POST",
+      body: JSON.stringify({
+        repositoryName: newRepositoryForm.repositoryName,
+        repositoryDescription: newRepositoryForm.repositoryDescription,
+        isPrivate: repositoryIsPrivate.value,
+      }),
+      headers: {
+        "access-token": userAuth.value.accessToken,
+      },
+      onResponse({ _, response }) {
+        Message.clear();
+        const message = response._data["message"];
+        if (response.status === HTTPStatusCode.OK) {
+          Message.success(message);
+          router.push("/");
+        } else {
+          Message.error(message);
+        }
+      },
+    });
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+const repositoryNameRules = [
+  {
+    validator: async (value, cb) => {
+      const config = useRuntimeConfig();
+      try {
+        const repositoryName = value ? value : "";
+        const userId = userInfo.value.id ? userInfo.value.id : "";
+        if (!userId) {
+          // 到底会不会出现这个问题呢
+          Message.error("校验仓库名时，前端出现内部错误");
+          cb();
+          return;
+        }
+        const apiURL = new URL(
+          APIPaths.REPOSITORY_CHECK_REPOSITORY_NAME_VALIDITY_API_PATH,
+          config.public.apiBaseURL
+        );
+        apiURL.searchParams.append("repositoryName", repositoryName);
+        apiURL.searchParams.append("userId", userId);
+        await $fetch(apiURL);
+        cb();
+      } catch (error) {
+        console.error(error);
+        const message = error.response._data["message"];
+        cb(message);
+      }
+    },
+  },
+];
+
+const newRepositoryFormRules = {
+  repositoryName: repositoryNameRules,
+};
 </script>
 
 <template>
@@ -16,7 +86,11 @@ const handleNewRepository = () => {};
     :style="{ width: newRepositoryWidth }"
   >
     <ATypographyTitle :heading="2"> 新建仓库 </ATypographyTitle>
-    <AForm :model="newRepositoryForm" layout="vertical">
+    <AForm
+      :model="newRepositoryForm"
+      layout="vertical"
+      :rules="newRepositoryFormRules"
+    >
       <AFormItem field="repositoryName" label="仓库名">
         <AInput v-model="newRepositoryForm.repositoryName" />
       </AFormItem>
@@ -25,8 +99,8 @@ const handleNewRepository = () => {};
       </AFormItem>
     </AForm>
     <ADivider />
-    <a-radio-group direction="vertical">
-      <a-radio class="mb-2">
+    <a-radio-group direction="vertical" v-model="repositoryIsPrivate">
+      <a-radio class="mb-2" :value="false">
         <div class="flex items-center gap-2 ml-2">
           <Icon name="fa6-solid:book-bookmark" size="24" />
           <div class="flex flex-col">
