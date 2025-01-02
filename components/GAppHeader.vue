5c6aef66cc028d22fc33d25012ad1eb649086182
Kaiser-Yang <624626089@qq.com>
2025-01-02 23:56:17 +0800

    Add scipt to fetch repositories when login success

--- components/GAppHeader.vue
+++ components/GAppHeader.vue
@@ -1,7 +1,11 @@
 <script setup>
+import { APIPaths } from "~/utils";
+
 const searchBarWidth = 300;
 
 const userAuth = useState("userAuth");
+const userInfo = useState("userInfo")
+const homepageRepositories = useState("homepageRepositories");
 
 const router = useRouter();
 
@@ -13,6 +17,51 @@ const handleNewOption = (v) => {
 
 const newOptions = {
   newRepository: "新建仓库",
+};
+
+const fetchRepositories = async () => {
+  const config = useRuntimeConfig();
+  const apiURL = new URL(
+    APIPaths.USER_PAGE_USER_REPOSITORY_API_PATH,
+    config.public.apiBaseURL
+  );
+  apiURL.searchParams.append("id", userInfo.value.id);
+  apiURL.searchParams.append("page", 1);
+  apiURL.searchParams.append("size", 10);
+  try {
+    await $fetch(apiURL, {
+      method: "GET",
+      headers: {
+        "access-token": userAuth.value.accessToken,
+      },
+      onResponse({ _, response }) {
+        homepageRepositories.value = response._data
+      },
+    });
+  } catch (error) {
+    console.error("获取仓库数据失败:", error);
+  }
+};
+
+watch(
+  () => userAuth?.value?.accessToken,
+  (newToken) => {
+    if (newToken) {
+      fetchRepositories();
+    }
+  },
+  { immediate: true }
+);
+const isLoading = ref(false);
+
+const cloneHttps = (url) => {
+  navigator.clipboard.writeText(url);
+  Message.success(`已复制 HTTPS URL: ${url}`)
+};
+
+const cloneSsh = (url) => {
+  navigator.clipboard.writeText(url);
+  Message.success(`已复制 SSH URL: ${url}`);
 };
 </script>
 
@@ -39,7 +88,7 @@ const newOptions = {
           </template>
         </AButton>
         <template #content>
-          <a-doption v-for="value in newOptions"> {{ value }} </a-doption>
+          <a-doption v-for="key in newOptions" :key="key"> {{ key }} </a-doption>
         </template>
       </ADropdown>
       <!-- TODO: 改为用户信息显示 -->
