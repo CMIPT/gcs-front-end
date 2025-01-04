aca876c12ce0c91452d2e0c28db158c11a4918ea
nonumx <nonumx@gmail.com>
2025-01-04 10:22:58 +0800

    Add main layout and placeholder pages for login/signup

--- pages/index.vue
+++ pages/index.vue
@@ -3,5 +3,33 @@
 </script>
 
 <template>
-  Hello
+  <a-layout>
+    <a-layout-header>
+      <div class="flex items-center h-16 px-6 py-4 border-b border-solid border-slate-200">
+        <!-- 网页图标 -->
+        <NuxtLink to="/">
+          <Icon name="fa6-solid:code" />
+        </NuxtLink>
+        <NuxtLink to="/" class="ml-4"> GCS </NuxtLink>
+        <div class="flex gap-2 items-center ml-auto">
+          <NuxtLink to="/auth/sign-in">
+            <AButton type="text">登录</AButton>
+          </NuxtLink>
+          <NuxtLink to="/auth/sign-up">
+            <AButton type="text">注册</AButton>
+          </NuxtLink>
+        </div>
+      </div>
+    </a-layout-header>
+    <a-layout-content>
+      <div class="text-center">
+        主页面
+      </div>
+    </a-layout-content>
+    <a-layout-footer>
+      <div class="flex justify-center items-center pt-10">
+        © 2024 GCS. All rights reserved
+      </div>
+    </a-layout-footer>
+  </a-layout>
 </template>
