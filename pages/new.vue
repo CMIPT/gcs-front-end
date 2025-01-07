9c1d61bfa48250c7854d5c4447134c1d8cc3e176
nonumx <nonumx@gmail.com>
2025-01-07 11:34:33 +0800

    Add new repository page

--- pages/new.vue
+++ pages/new.vue
@@ -1,6 +1,119 @@
 <script setup lang="ts">
+import type { FieldRule } from '@arco-design/web-vue';
+
+const formMaxWidth = '1000px'
+
+type NewRepositoryForm = {
+  name: string,
+  description: string,
+  isPrivate: boolean,
+}
+
+const userAuth = useUserAuth()
+const userInfo = useUserInfo()
+const router = useRouter()
+
+const form = reactive<NewRepositoryForm>({
+  name: "",
+  description: "",
+  isPrivate: false
+})
+
+const nameRules: FieldRule[] = [{
+  validator: async (value, cb) => {
+    const repositoryName = value || ""
+    const apiURL = new URL(
+      APIPaths.REPOSITORY_CHECK_REPOSITORY_NAME_VALIDITY_API_PATH,
+      window.origin
+    );
+    apiURL.searchParams.append("repositoryName", repositoryName);
+    apiURL.searchParams.append("userId", userInfo.value.id);
+    await $fetch(apiURL.toString()).catch((error) => {
+      const message = error.data["message"];
+      cb(message);
+    })
+  }
+}]
+
+const formRules = {
+  name: nameRules
+}
+
+const handleNewRepository = () => {
+  Message.loading({ id: "new-repository", content: "正在新建仓库..." });
+  const apiURL = new URL(
+    APIPaths.REPOSITORY_CREATE_REPOSITORY_API_PATH,
+    window.origin
+  );
+  $fetch(apiURL.toString(), {
+    method: "POST",
+    body: JSON.stringify({
+      repositoryName: form.name,
+      repositoryDescription: form.description,
+      isPrivate: form.isPrivate,
+    }),
+    headers: {
+      "access-token": userAuth.value?.accessToken || "",
+    },
+  })
+    .then(() => {
+      Message.success({ id: "new-repository", content: "创建成功，跳转到主页" });
+      router.push("/");
+    }).catch((error) => {
+      const message = error.data["message"];
+      Message.error({ id: "new-repository", content: message });
+    });
+};
 </script>
 
 <template>
-  新建仓库
+  <div class="flex flex-col items-center mx-auto my-10">
+    <a-typography-title :heading="2"> 新建仓库 </a-typography-title>
+    <a-form :model="form" layout="vertical" :rules="formRules" :style="{ maxWidth: formMaxWidth }"
+      @submit-success="handleNewRepository">
+      <a-form-item field="name" label="仓库名">
+        <a-input v-model="form.name" />
+      </a-form-item>
+      <a-form-item field="description" label="仓库描述">
+        <a-input v-model="form.description" />
+      </a-form-item>
+      <a-divider />
+      <a-form-item field="isPrivate">
+        <a-radio-group direction="vertical" v-model="form.isPrivate">
+          <a-radio :value="false">
+            <div class="flex items-center gap-2 ml-2">
+              <Icon name="fa6-solid:book-bookmark" size="24" />
+              <div class="flex flex-col">
+                <ATypographyText class="text-base font-bold">
+                  公开仓库
+                </ATypographyText>
+                <ATypographyText>
+                  任何人都可以查看这个仓库，由你控制代码提交。
+                </ATypographyText>
+              </div>
+            </div>
+          </a-radio>
+          <a-radio :value="true">
+            <div class="flex items-center gap-2 ml-2">
+              <Icon name="fa6-solid:lock" size="24" />
+              <div class="flex flex-col">
+                <ATypographyText class="text-base font-bold">
+                  私密仓库
+                </ATypographyText>
+                <ATypographyText>
+                  由你控制仓库查看和代码提交的权利。
+                </ATypographyText>
+              </div>
+            </div>
+          </a-radio>
+        </a-radio-group>
+      </a-form-item>
+      <a-divider />
+      <a-form-item>
+        <a-button class="ml-auto" type="primary" html-type="submit">
+          创建仓库
+        </a-button>
+      </a-form-item>
+    </a-form>
+  </div>
 </template>
