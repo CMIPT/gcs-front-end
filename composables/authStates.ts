4261e1be697eed313bf710de4700c42da9edb42b
Kaiser-Yang <624626089@qq.com>
2025-02-22 23:23:56 +0800

    Re-fetch when access-token expired

--- composables/authStates.ts
+++ composables/authStates.ts
@@ -1,5 +1,3 @@
-import type { UserVO } from "./responseVOTypes";
-
 export type UserAuthState = {
   accessToken: string | null;
   refreshToken: string | null;
@@ -7,16 +5,12 @@ export type UserAuthState = {
 
 export const useUserAuth = () => {
   return useState<UserAuthState>("userAuth", () => {
-    return {
-      accessToken: null,
-      refreshToken: null,
-    };
+    return {} as UserAuthState;
   });
 };
 
 export const useUserInfo = () => {
   return useState<UserVO>("userInfo", () => {
-    // TODO: Fetch user info from the server by token
-    return { id: "", username: "", email: "" };
+    return {} as UserVO;
   });
 };
