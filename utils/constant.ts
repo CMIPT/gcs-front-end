dfa923d88b41b6a6f2a7fdc709428b7672d05f62
Kaiser-Yang <624626089@qq.com>
2025-02-22 23:08:18 +0800

    Rename files

--- utils/constant.ts
+++ utils/constant.ts
@@ -70,8 +70,9 @@ export const APIErrorCodes = {
   INVALID_EMAIL_VERIFICATION_CODE: "30",
 };
 
-export const HTTPStatusCode = {
+export const HTTPStatus = {
   OK: 200,
   BAD_REQUEST: 400,
+  UNAUTHORIZED: 401,
   INTERNAL_SERVER_ERROR: 500,
 };
