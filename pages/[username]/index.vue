f5b18b9e6dfa20edc18490b78991cae9cfb90c00
Kaiser-Yang <624626089@qq.com>
2025-02-23 18:46:52 +0800

    Rename route, user's home page

--- pages/[username]/index.vue
+++ pages/[username]/index.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import type { PaginationProps } from "@arco-design/web-vue";
 
-const userAuth = useUserAuth();
+const route = useRoute();
 const userInfo = useUserInfo();
+const username = ref<string>(route.params.username as string);
 
 const userRepositoryList = ref<RepositoryVO[]>([]);
 const total = ref(0);
@@ -10,15 +11,11 @@ const currentPage = ref(1);
 const defaultPageSize = ref(10);
 
 const fetchRepositories = async (page: number) => {
-  if (!userInfo.value.id) {
-    // 还没有登录状态时不查询仓库列表
-    return;
-  }
   const apiURL = new URL(
     APIPaths.REPOSITORY_PAGE_REPOSITORY_API_PATH,
     window.origin,
   );
-  apiURL.searchParams.append("id", userInfo.value.id);
+  apiURL.searchParams.append("username", username.value);
   apiURL.searchParams.append("page", page.toString());
   apiURL.searchParams.append("size", defaultPageSize.value.toString());
   const response = await fetchWithRetry<PageVO<RepositoryVO>>(
@@ -30,6 +27,9 @@ const fetchRepositories = async (page: number) => {
 
 onMounted(async () => {
   await initialize();
+  if (!userInfo.value.id) {
+    useRouter().push("/");
+  }
   fetchRepositories(currentPage.value);
 });
 
@@ -57,18 +57,7 @@ const paginationProps = computed(() => {
           <Icon name="fa6-solid:code" />
         </NuxtLink>
         <NuxtLink to="/" class="ml-4"> GCS </NuxtLink>
-        <div
-          class="flex gap-2 items-center ml-auto"
-          v-if="!userAuth?.accessToken"
-        >
-          <NuxtLink to="/login">
-            <AButton type="text">登录</AButton>
-          </NuxtLink>
-          <NuxtLink to="/signup">
-            <AButton type="text">注册</AButton>
-          </NuxtLink>
-        </div>
-        <div class="flex gap-2 items-center ml-auto" v-else>
+        <div class="flex gap-2 items-center ml-auto">
           <NuxtLink to="/new">
             <a-button type="text">新建仓库</a-button>
           </NuxtLink>
@@ -80,7 +69,7 @@ const paginationProps = computed(() => {
     </a-layout-header>
     <a-layout-content>
       <div class="max-w-screen-lg mx-auto">
-        <a-typography-title :heading="3"> 项目 </a-typography-title>
+        <a-typography-title :heading="3"> {{ username }} 的项目 </a-typography-title>
         <a-list
           :bordered="false"
           :data="userRepositoryList"
