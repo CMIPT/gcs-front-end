bc91ae658a40b6e99b74a091f315097c09ccb1b7
Kaiser-Yang <624626089@qq.com>
2025-02-24 20:31:42 +0800

    Settings page and logout page

---
+++ pages/(auth)/logout.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const router = useRouter();
+const userInfo = useUserInfo();
+onMounted(async () => {
+  if (userInfo.value.id) {
+    const apiURL = new URL(
+      APIPaths.AUTHENTICATION_SIGN_OUT_API_PATH,
+      window.origin,
+    );
+    apiURL.searchParams.append("id", userInfo.value.id);
+    await $fetch(apiURL.toString(), {
+      method: "DELETE",
+    })
+      .then(() => {
+        sessionStorage.removeItem("access-token");
+        localStorage.removeItem("refresh-token");
+        userInfo.value = {} as UserVO;
+        useUserAuth().value = {} as UserAuthState;
+        Message.success("你已成功登出");
+      })
+      .catch((error) => {
+        Message.error(error.data.message);
+      });
+  }
+  router.push("/");
+});
+</script>
